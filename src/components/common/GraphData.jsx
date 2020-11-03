bbeeafbbd143156cedf03ce47d075576ec93456a
Galo Sandoval <galosan92@gmail.com>
2020-11-02 17:17:50 -0800

    adds some styling to the graph

---
+++ src/components/common/GraphData.jsx
@@ -0,0 +1,10 @@
+export const GraphData = [
+  { x: new Date(1982, 1, 1), y: 125, label:'a' },
+  { x: new Date(1987, 1, 1), y: 257, label:'a' },
+  { x: new Date(1993, 1, 1), y: 345, label:'a' },
+  { x: new Date(1997, 1, 1), y: 515, label:'a' },
+  { x: new Date(2001, 1, 1), y: 132, label:'a' },
+  { x: new Date(2005, 1, 1), y: 305, label:'a' },
+  { x: new Date(2011, 1, 1), y: 270, label:'a' },
+  { x: new Date(2015, 1, 1), y: 470, label:'a' },
+]
--- src/components/common/NewGraph.js
+++ src/components/common/NewGraph.js
@@ -5,12 +5,17 @@ import {
   VictoryAxis,
   VictoryLine,
   VictoryBrushContainer,
+  VictoryTooltip,
+  VictoryScatter,
 } from 'victory';
+
+import './About.css';
+import { GraphData } from './GraphData';
 
 class NewGraph extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { isShown: false };
   }
 
   handleZoom(domain) {
@@ -23,7 +28,7 @@ class NewGraph extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="graph-container">
         <VictoryChart
           width={1000}
           height={600}
@@ -38,24 +43,46 @@ class NewGraph extends React.Component {
           }
         >
           <VictoryLine
+            labelComponent={<VictoryTooltip />}
             style={{
               data: { stroke: 'tomato' },
             }}
-            data={[
-              { x: new Date(1982, 1, 1), y: 125 },
-              { x: new Date(1987, 1, 1), y: 257 },
-              { x: new Date(1993, 1, 1), y: 345 },
-              { x: new Date(1997, 1, 1), y: 515 },
-              { x: new Date(2001, 1, 1), y: 132 },
-              { x: new Date(2005, 1, 1), y: 305 },
-              { x: new Date(2011, 1, 1), y: 270 },
-              { x: new Date(2015, 1, 1), y: 470 },
+            data={GraphData}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onMouseOver: () => {
+                    return [
+                      {
+                        mutation: props => {
+                          return {
+                            style: Object.assign({}, props.style, {
+                              fill: 'tomato',
+                            }),
+                          };
+                        },
+                      },
+                    ];
+                  },
+                  onMouseOut: () => {
+                    return [
+                      {
+                        mutation: () => {
+                          return null;
+                        },
+                      },
+                    ];
+                  },
+                },
+              },
             ]}
           />
+          <VictoryScatter data={GraphData} />
         </VictoryChart>
 
         <VictoryChart
-          width={550}
+          width={800}
           height={90}
           scale={{ x: 'time' }}
           padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
