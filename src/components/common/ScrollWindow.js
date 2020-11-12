feb1fde0d2d0549bae7a4902e06d9b8cd5e1bdfa
Galo Sandoval <galosan92@gmail.com>
2020-11-12 10:16:18 -0800

    adds styling to scroll container and checkboxes

--- src/components/common/ScrollWindow.js
+++ src/components/common/ScrollWindow.js
@@ -3,33 +3,45 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const SWDiv = styled.div`
-  height: 170px;
-  width: 1000px;
+  height: 50vh;
+  width: 100%;
   background-color: lightblue;
-  overflow: scroll;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
 `;
 
 const ScrollWindow = props => {
   const { data } = props;
   return (
     <SWDiv>
-      {data.map(incidents => {
-        const linksArr = incidents.src.split('');
-        linksArr.splice(0, 1);
-        linksArr.splice(linksArr.length - 1, 1);
-        linksArr.splice(0, 1);
-        linksArr.splice(linksArr.length - 1, 1);
+      {data.length === 0 && (
+        <p>
+          Click a node on the graph to display instances of police violence per
+          month
+        </p>
+      )}
+      {data.map(incident => {
+        const linksArr = incident.src.split('');
+        linksArr.splice(0, 2);
+        linksArr.splice(linksArr.length - 1, 2);
         let newLink = Array(linksArr.join('').split(','));
-
         return (
           <div>
+            <h1>{incident.title}</h1>
+            <h2>
+              {incident.city}, {incident.state}
+            </h2>
             {newLink[0].map((link, index) => {
               if (link.includes('http')) {
                 return (
-                  <Link to={link} target="_blank">{`Video ${index + 1}`}</Link>
+                  <Link to={link} target="_blank">{`Source ${index + 1}`}</Link>
                 );
               }
             })}
+            <p>{incident.description}</p>
           </div>
         );
       })}
